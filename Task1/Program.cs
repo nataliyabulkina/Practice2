7254fcfc8850f793977b07bcb0cd966550906fc7
Name <your.Nataliyabulkina1410@gmail.com>
2022-09-10 10:29:09 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = number1 / 10 % 10;
+Console.WriteLine($"Вторая цифра: {number2}");
+
